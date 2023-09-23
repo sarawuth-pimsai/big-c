54330d4ae24a7adf4c31ae07e2f68347d02804cd
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2023-09-23 13:20:02 +0700

    fix (assignment_3): Array Sample 1 is success

--- assignments/assignment_3.ts
+++ assignments/assignment_3.ts
@@ -1,22 +1,27 @@
-function recursion(values) {
-  let stacks = []
-  //   let level = 0
-  //   let index = 1
-
-  for (const level of Object.keys(values)) {
-    if (Array.isArray(values[level])) {
-      for (const index of Object.keys(values[level])) {
-        console.log(`L${level} I${index}`)
-      }
+function recursion(values, level, lindex, index, rows, outputs, loop) {
+  if (level >= values.length || !values[level][index]) return
+  rows.push(values[level][index])
+  if (Array.isArray(values[level + 1])) {
+    recursion(values, level + 1, lindex, index, rows, outputs, 'level')
+    if (values[level][lindex + 1]) {
+      const newRows = rows.slice(0, level)
+      newRows.push(values[level][lindex + 1])
+      // console.log({ loop: 'Change Level', newRows })
+      recursion(values, level, lindex + 1, 0, newRows, outputs, loop)
     }
+  } else {
+    const newRows = rows.slice(0, level)
+    newRows.push(values[level][index])
+    console.log({ newRows })
+    recursion(values, level, lindex, index + 1, newRows, outputs, 'index')
   }
 }
-
 const inputs = [
   [[1], [2], [3, 4], [5, 6], [7, 8]],
   [[1], [2, 3], [4, 5, 6]],
 ]
-let outputs: number[] = []
-recursion(inputs[1] as [])
+let outputs = []
+recursion(inputs[1], 0, 0, 0, [], outputs, 'level')
+// console.log({ count: outputs.length, outputs })
 // console.log(outputs)
 // console.log({ input: inputs[1], outputs })
